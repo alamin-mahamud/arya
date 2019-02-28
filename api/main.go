104eb62fed0733f2b69037d1687b8e5cf272fca7
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-02-28 19:49:42 +0600

    app config

--- api/main.go
+++ api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/alamin-mahamud/arya/api/pkg"
 )
@@ -9,7 +10,12 @@ import (
 const port = "8080"
 
 func main() {
+	config, err := pkg.LoadConfig("./config")
+	if err != nil {
+		panic("Could not load the configuration")
+	}
+
 	r := pkg.NewRouter()
 	http.Handle("/", r)
-	http.ListenAndServe(":"+port, nil)
+	http.ListenAndServe(":"+strconv.Itoa(config.ServerPort), nil)
 }
---
+++ api/pkg/config.go
@@ -0,0 +1,62 @@
+package pkg
+
+import (
+	"fmt"
+
+	"github.com/go-ozzo/ozzo-validation"
+	"github.com/spf13/viper"
+)
+
+type AppConfig struct {
+	// the path to the error message file. Defaults to "config/errors.yaml"
+	ErrorFile string `mapstructure:"error_file"`
+	// the server port. Defaults to 8080
+	ServerPort int `mapstructure:"server_port"`
+	// the data source name (DSN) for connecting to the database. required.
+	DSN string `mapstructure:"dsn"`
+	// the signing method for JWT. Defaults to "HS256"
+	JWTSigningMethod string `mapstructure:"jwt_signing_method"`
+	// JWT signing key. required.
+	JWTSigningKey string `mapstructure:"jwt_signing_key"`
+	// JWT verification key. required.
+	JWTVerificationKey string `mapstructure:"jwt_verification_key"`
+}
+
+func (config AppConfig) validate() error {
+	return validation.ValidateStruct(&config,
+		validation.Field(&config.DSN, validation.Required),
+		validation.Field(&config.JWTSigningKey, validation.Required),
+		validation.Field(&config.JWTVerificationKey, validation.Required),
+	)
+}
+
+func LoadConfig(configPaths ...string) (*AppConfig, error) {
+
+	cfg := AppConfig{}
+	v := viper.New()
+	v.SetConfigName("app")
+	v.SetConfigType("yaml")
+	v.SetEnvPrefix("arya")
+	v.AutomaticEnv()
+	v.SetDefault("error_file", "config/errors.yaml")
+	v.SetDefault("server_port", 8080)
+	v.SetDefault("jwt_signing_method", "HS256")
+
+	for _, path := range configPaths {
+		v.AddConfigPath(path)
+	}
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("Failed to read the configuration file: %s", err)
+	}
+
+	if err := v.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("Could not unmarshal into `config`")
+	}
+
+	if err := cfg.validate(); err != nil {
+		return nil, fmt.Errorf("Configuration Validation Failed. %s", err)
+	}
+
+	return &cfg, nil
+}
